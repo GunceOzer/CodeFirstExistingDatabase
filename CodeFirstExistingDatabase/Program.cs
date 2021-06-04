ed1cc798151f60eab70979b6a0ca7716790ebb64
GunceOzer <guncedemirel.ozer@ogr.deu.edu.tr>
2021-06-04 13:33:12 +0300

    Add project files.

---
+++ CodeFirstExistingDatabase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExistingDatabase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dbContext = new PlutoContext();
+			var course = new Course()
+			{
+				Id = 3,
+				Title="Code First",
+				Description ="Code first example with existing database",
+				Level = 2,
+				FullPrice = (float)36.0,
+				
+
+
+
+			};
+			dbContext.Courses.Add(course);
+			dbContext.SaveChanges();
+			Console.WriteLine(course.Description);
+			
+		}
+	}
+}
